420f7695bf26bcff2c799d163aa7ab7662595363
Muramatsu <62125008+Glider2355@users.noreply.github.com>
2026-01-14 00:52:12 +0900

    refactor

--- integration-test/fixtures/src/UserList.tsx
+++ integration-test/fixtures/src/UserList.tsx
@@ -4,10 +4,10 @@ import type { paths } from "./schema";
 const api = createClient<paths>();
 
 export function UserList() {
-  const fetchUsers = async () => {
-    const { data } = await api.GET("/users");
-    return data;
-  };
+	const fetchUsers = async () => {
+		const { data } = await api.GET("/users");
+		return data;
+	};
 
-  return <div>Users</div>;
+	return <div>Users</div>;
 }
